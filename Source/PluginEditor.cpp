1a8c2f171d6804bc54ce6a6c460f5656cb4680d3
fan7asy <blkmhntr@gmail.com>
2022-08-25 20:24:40 -0700

    Analyzer decibels marks added and code cleanup

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -407,6 +407,15 @@ void ResponseCurveComponent::resized()
         g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::lightgrey);
 
         g.drawFittedText(str, r, juce::Justification::centred, 1);
+
+        str.clear();
+        str << (gDb - 24.f);
+
+        r.setX(1);
+        textWidth = g.getCurrentFont().getStringWidth(str);
+        r.setSize(textWidth, fontHeight);
+        g.setColour(Colours::lightgrey);
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
     }
 }
 
