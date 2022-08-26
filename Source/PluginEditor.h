048703ffe2f4fca3aeccbefbee308146176bca0b
fan7asy <blkmhntr@gmail.com>
2022-08-25 20:03:08 -0700

    Added grid lines in background image

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -69,6 +69,8 @@ struct ResponseCurveComponent : juce::Component,
     void timerCallback() override;
 
     void paint(juce::Graphics& g) override;
+    void resized() override;
+
 private:
     SimpleEQAudioProcessor& audioProcessor;
     juce::Atomic<bool> parametersChanged{ false };
@@ -76,6 +78,12 @@ private:
     MonoChain monoChain;
 
     void updateChain();
+
+    juce::Image background;
+
+    juce::Rectangle<int> getRenderArea();
+
+    juce::Rectangle<int> getAnalysisArea();
 };
 
 //==============================================================================
