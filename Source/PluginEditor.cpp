9e4157cde80ac39b62cb7da54860fc1028b76a6a
fan7asy <blkmhntr@gmail.com>
2022-08-26 23:28:23 -0700

    Toggle button graphics added

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -65,6 +65,44 @@ void LookAndFeel::drawRotarySlider(juce::Graphics& g,
     }
 
 }
+
+void LookAndFeel::drawToggleButton(juce::Graphics& g,
+    juce::ToggleButton& toggleButton,
+    bool shouldDrawButtonAsHighlighted,
+    bool shouldDrawButtonAsDown)
+{
+    using namespace juce;
+
+    Path powerButton;
+
+    auto bounds = toggleButton.getLocalBounds();
+    auto size = jmin(bounds.getWidth(), bounds.getHeight()) - 3;
+    auto r = bounds.withSizeKeepingCentre(size, size).toFloat();
+
+    float ang = 40.f;
+
+    size -= 3;
+
+    powerButton.addCentredArc(r.getCentreX(),
+        r.getCentreY(),
+        size * 0.5,
+        size * 0.5,
+        0.f,
+        degreesToRadians(ang),
+        degreesToRadians(360.f - ang),
+        true);
+    
+    powerButton.startNewSubPath(r.getCentreX(), r.getY());
+    powerButton.lineTo(r.getCentre());
+
+    PathStrokeType pst(3.f, PathStrokeType::JointStyle::curved);
+
+    auto color = toggleButton.getToggleState() ? Colours::dimgrey : Colour(0u, 172u, 1u);
+
+    g.setColour(color);
+    g.strokePath(powerButton, pst);
+    //g.drawEllipse(r, 2); //circle around power button
+}
 //==============================================================================
 void RotarySliderWithLabels::paint(juce::Graphics& g)
 {
@@ -563,6 +601,10 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor(SimpleEQAudioProcesso
     {
         addAndMakeVisible(comp);
     }
+
+    peakBypassButton.setLookAndFeel(&lnf);
+    lowcutBypassButton.setLookAndFeel(&lnf);
+    highcutBypassButton.setLookAndFeel(&lnf);
 
     setSize (600, 480);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -154,6 +154,11 @@ struct LookAndFeel : juce::LookAndFeel_V4
         float rotaryStartAngle,
         float rotaryEndAngle,
         juce::Slider&) override;
+
+    void drawToggleButton(juce::Graphics& g,
+        juce::ToggleButton& toggleButton,
+        bool shouldDrawButtonAsHighlighted,
+        bool shouldDrawButtonAsDown) override;
 };
 
 struct RotarySliderWithLabels : juce::Slider
@@ -298,6 +303,8 @@ private:
 
 
     std::vector<juce::Component*> getComps();
+
+    LookAndFeel lnf;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
